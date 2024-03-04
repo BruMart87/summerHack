35eed12ac84c085396654ed8127197bfea7f70b4
Bruno Martino <brunomartino1987@gmail.com>
2024-03-04 16:58:20 -0300

    mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MiPrimerComponente from './MiPrimerComponente'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>Hola desde React</h1>
+      <p>boca la concha de tu madreeeee</p>
+      <MiPrimerComponente></MiPrimerComponente>
+    </>
+  )
+}
+
+export default App
---
+++ src/MiPrimerComponente.jsx
@@ -0,0 +1,7 @@
+function miPrimerComponente() {
+    return(
+        <h1>Mi primer componente, hola!</h1>
+    )
+}
+
+export default miPrimerComponente;
